e1b7b1aece4449b33d2271c8fd039f1a53ace460
AuRocKbra <4haurock@gmail.com>
2020-10-19 15:28:45 -0300

    Primeiro Commmit

---
+++ src/test/java/com/aurock/workshop/mongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurock.workshop.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
