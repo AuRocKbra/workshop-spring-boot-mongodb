7a75d59f2f013e09b4d910ff752636f0e6651e69
AugustoEs <augustoespindoal@canaltecnico.com.br>
2020-10-28 17:37:52 -0300

    Busca de post por ID

---
+++ src/main/java/com/aurock/workshop/mongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.aurock.workshop.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aurock.workshop.mongo.domain.Post;
+import com.aurock.workshop.mongo.repository.PostRepository;
+import com.aurock.workshop.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		try {
+			Optional<Post> user = postRepository.findById(id);
+			return user.get();
+		}catch(RuntimeException e ) {
+			throw new ObjectNotFoundException("Recurso não encontrado");
+		}
+	}
+}
