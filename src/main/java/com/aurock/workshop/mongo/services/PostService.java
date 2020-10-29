ab4571a87774a572e1fc721785369c16f076b583
AugustoEs <augustoespindoal@canaltecnico.com.br>
2020-10-29 15:54:03 -0300

    Implementado metodo com Query method e @Query

--- src/main/java/com/aurock/workshop/mongo/services/PostService.java
+++ src/main/java/com/aurock/workshop/mongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.aurock.workshop.mongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,9 @@ public class PostService {
 		}catch(RuntimeException e ) {
 			throw new ObjectNotFoundException("Recurso não encontrado");
 		}
+	}
+	
+	public List<Post> findByTitle(String txt){
+		return postRepository.searchTitle(txt);
 	}
 }
